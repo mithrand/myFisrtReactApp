078ff4fe5e73a80008e78f10b1be809d03cdb4f2
Antonio Muñoz <munoztorres.antonio@gmail.com>
2017-02-19 01:38:02 +0100

    Kickoff

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+/**
+ * Created by Antonio on 19/02/2017.
+ */
+
+
+import greeting from "./modulo";
+
+console.log(greeting);
---
+++ src/modulo.tsx
@@ -0,0 +1,8 @@
+/**
+ * Created by Antonio on 19/02/2017.
+ */
+
+
+const greeting: string = "Hello World!";
+
+export default greeting;
